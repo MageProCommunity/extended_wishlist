ff4e1ed225e93b54c34116a371579e90f2e680f2
pavelivacko <iv.pafa@gmail.com>
2022-02-12 22:23:49 +0200

    Added uninstall process for module tables

---
+++ Setup/Uninstall.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace MagePro\ExtendedWishlis\Setup;
+
+use Magento\Framework\Setup\UninstallInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use MagePro\ExtendedWishlist\Exception\Setup\UninstallPatchException;
+
+class Uninstall implements UninstallInterface
+{
+    private const LIST_MAGE_PRO_WISHLIST_TABLES = [
+        'extended_wishlist',
+        'extended_wishlist_store',
+        'extended_wishlist_customer',
+        'extended_wishlist_product'
+    ];
+
+    /**
+     * Remove data that was created during module installation.
+     *
+     * @param SchemaSetupInterface   $installer
+     * @param ModuleContextInterface $context
+     *
+     * @return void
+     */
+    public function uninstall(SchemaSetupInterface $installer, ModuleContextInterface $context): void
+    {
+        $installer->startSetup();
+
+        /** @var AdapterInterface $connection */
+        $connection = $installer->getConnection();
+
+        try {
+            foreach (self::LIST_MAGE_PRO_WISHLIST_TABLES as $wishlistTable) {
+                $connection->dropTable($wishlistTable);
+            }
+        } catch (UninstallPatchException $exception) {
+
+        }
+
+        $installer->endSetup();
+    }
+}
